36e973434e5a913835abdac7035014a6f25f4338
Hardi <hardivirani2003@gmail.com>
2026-06-25 14:56:29 -0400

    first commit

---
+++ src/test/java/com/star/SpringEcomWebApp/SpringEcomWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.star.SpringEcomWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEcomWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
