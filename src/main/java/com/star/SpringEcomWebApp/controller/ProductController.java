8035c5c59b6a0ec5a7ee69b7ac3853ecbe24bac7
Hardi <hardivirani2003@gmail.com>
2026-06-25 18:00:32 -0400

    Vid - 109 : Add Product with Image

--- src/main/java/com/star/SpringEcomWebApp/controller/ProductController.java
+++ src/main/java/com/star/SpringEcomWebApp/controller/ProductController.java
@@ -7,7 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -34,6 +36,20 @@ public class ProductController {
        else {
            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
        }
+    }
+
+    @PostMapping("/product")
+    public ResponseEntity<?> addProduct(@RequestPart Product product, @RequestPart MultipartFile imageFile) {
+        Product savedProduct = null;
+        try {
+            savedProduct = productService.addProduct(product, imageFile);
+            return new ResponseEntity<>(savedProduct, HttpStatus.CREATED);
+        } catch (IOException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
 
     }
+
+
+
 }
--- src/main/java/com/star/SpringEcomWebApp/model/Product.java
+++ src/main/java/com/star/SpringEcomWebApp/model/Product.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -29,9 +30,15 @@ public class Product {
     private String brand;
     private BigDecimal price;
     private String category;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-mm-yyyy") //to have a perfect date i want, i my frontend.
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy") //to have a perfect date i want, i my frontend.
     private Date releaseDate;
     private boolean productAvailable;
     private int stockQuantity;
+    private String imageName;
+    private String imageType;
+    @Lob //Large object.
+    private  byte[] imageData; //byte[] because in database, we have to convert the image.
+
+
 
 }
