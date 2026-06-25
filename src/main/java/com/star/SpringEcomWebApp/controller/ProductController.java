33a06ea086dc15245a20ece213702e50a36afc6c
Hardi <hardivirani2003@gmail.com>
2026-06-25 16:57:46 -0400

    Vid - 102, 103, 104, 105, 106, 107 - All done by myself by pratice, previous github was messed up so.

---
+++ src/main/java/com/star/SpringEcomWebApp/controller/ProductController.java
@@ -0,0 +1,40 @@
+package com.star.SpringEcomWebApp.controller;
+
+import com.star.SpringEcomWebApp.model.Product;
+import com.star.SpringEcomWebApp.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> getAllProducts() {
+        return new ResponseEntity<>(productService.getAllProducts(), HttpStatus.OK);
+    }
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable int id) {
+
+        Product product = productService.getProductById(id);
+
+       if(product != null) {
+           return new ResponseEntity<>(product, HttpStatus.OK );
+       }
+       else {
+           return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+       }
+
+
+    }
+}
---
+++ src/main/java/com/star/SpringEcomWebApp/model/Product.java
@@ -0,0 +1,32 @@
+package com.star.SpringEcomWebApp.model;
+
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Product {
+
+    @Id
+    private int id;
+    private String name;
+    private String description;
+    private String brand;
+    private BigDecimal price;
+    private String category;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-mm-yyyy") //to have a perfect date i want, i my frontend.
+    private Date releaseDate;
+    private boolean productAvailable;
+    private int stockQuantity;
+}
