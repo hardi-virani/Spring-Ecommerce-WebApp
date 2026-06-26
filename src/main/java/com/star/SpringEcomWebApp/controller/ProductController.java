73bb32bfc7ca1542193bf3446204ea30e6719213
Hardi <hardivirani2003@gmail.com>
2026-06-25 20:38:13 -0400

    Vid - 110 & 111 : Fetch Product Image & Update& Delete Product

--- src/main/java/com/star/SpringEcomWebApp/controller/ProductController.java
+++ src/main/java/com/star/SpringEcomWebApp/controller/ProductController.java
@@ -4,17 +4,19 @@ import com.star.SpringEcomWebApp.model.Product;
 import com.star.SpringEcomWebApp.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.xml.transform.OutputKeys;
 import java.io.IOException;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin
+@CrossOrigin (origins = "http://localhost:5173")
 public class ProductController {
 
     @Autowired
@@ -30,7 +32,7 @@ public class ProductController {
 
         Product product = productService.getProductById(id);
 
-       if(product.getId() > 0) {
+       if(product.getId() >= 0) {
            return new ResponseEntity<>(product, HttpStatus.OK );
        }
        else {
@@ -38,16 +40,51 @@ public class ProductController {
        }
     }
 
+    @GetMapping("/product/{productId}/image")
+    public ResponseEntity<byte[]> getImageByProductId(@PathVariable int productId) {
+        Product product = productService.getProductById(productId);
+        if(product.getId() >= 0) {
+            return new ResponseEntity<>(product.getImageData(), HttpStatus.OK );
+        }
+        else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
     @PostMapping("/product")
-    public ResponseEntity<?> addProduct(@RequestPart Product product, @RequestPart MultipartFile imageFile) {
+    public ResponseEntity<?> addProduct(@RequestPart Product product, @RequestPart MultipartFile imageFile) { //imageFile comes from the react. We need to make the variable which is in the react or any frontend app.
         Product savedProduct = null;
         try {
-            savedProduct = productService.addProduct(product, imageFile);
+            savedProduct = productService.addOrUpdateProduct(product, imageFile);
             return new ResponseEntity<>(savedProduct, HttpStatus.CREATED);
         } catch (IOException e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
 
+    @PutMapping("/product/{id}")
+    public ResponseEntity<?> updateProduct(@PathVariable int id, @RequestPart Product product, @RequestPart MultipartFile imageFile) {
+        Product updatedProduct = null;
+
+        try {
+            updatedProduct = productService.addOrUpdateProduct(product, imageFile);
+            return new ResponseEntity<>("Updated", HttpStatus.OK);
+        } catch (IOException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping("/product/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable int id) {
+        Product product = productService.getProductById(id);
+
+        if(product != null) {
+            productService.deleteProduct(id);
+            return new ResponseEntity<>("Deleted", HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
 
