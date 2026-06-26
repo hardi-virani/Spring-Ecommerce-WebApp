412f97fd5232a855c74df5a2db288a9e4819269e
Hardi <hardivirani2003@gmail.com>
2026-06-26 15:18:18 -0400

    Vid - 117 - Creating Models for order

---
+++ src/main/java/com/star/SpringEcomWebApp/model/OrderItem.java
@@ -0,0 +1,32 @@
+package com.star.SpringEcomWebApp.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder //to build the objects (Ask AI)
+public class OrderItem {
+
+    @Id
+    private int id;
+
+    @ManyToOne
+    private Product product;
+
+    private int quantity;
+
+    private BigDecimal price;
+
+    @ManyToOne(fetch= FetchType.LAZY)
+    private Order order;
+
+
+}
