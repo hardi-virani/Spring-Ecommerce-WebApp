fee32a5b4fcf3955c11040acb45f95d8c6b3a262
Hardi <hardivirani2003@gmail.com>
2026-06-26 15:43:44 -0400

    Vid - 118 : Creating Order Controller

---
+++ src/main/java/com/star/SpringEcomWebApp/service/OrderService.java
@@ -0,0 +1,20 @@
+package com.star.SpringEcomWebApp.service;
+
+import com.star.SpringEcomWebApp.model.dto.OrderRequest;
+import com.star.SpringEcomWebApp.model.dto.OrderResponse;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrderService {
+
+    public OrderResponse placeOrder(OrderRequest orderRequest) {
+        return null;
+    }
+
+
+    public List<OrderResponse> getAllOrdersResponses() {
+        return null;
+    }
+}
